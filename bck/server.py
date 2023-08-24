9934fac110597f5d73d5deb8aa0191ccb9983a3c
fathisahar <sahar.fathi@emailgmail.info>
2023-08-24 13:19:04 -0400

    post api

--- bck/server.py
+++ bck/server.py
@@ -1,26 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import datetime
 
 x = datetime.datetime.now()
 
-# Initializing flask app
 app = Flask(__name__)
 CORS(app)
 
-# Route for seeing a data
-@app.route('/data')
-def get_time():
+# Sample initial data
+user_data = {
+    'Name': "geek",
+    'Age': "22",
+    'Date': x,
+    'programming': "python"
+}
 
-	# Returning an api for showing in reactjs
-	return {
-		'Name':"geek",
-		"Age":"22",
-		"Date":x,
-		"programming":"python"
-		}
+@app.route('/data', methods=['GET', 'POST'])
+def handle_data():
+    if request.method == 'GET':
+        return jsonify(user_data)
+    elif request.method == 'POST':
+        # Assuming the POST data is in JSON format
+        new_data = request.json
 
-	
-# Running app
+        # Update user_data with new_data
+        user_data.update(new_data)
+
+        return jsonify(message="Data updated successfully")
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
--- src/CreateUserPage.js
+++ src/CreateUserPage.js
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from "react";
 
 function CreateUserPage() {
-
     const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); 
-        setInputValue('');
-      };
+    const submitUser = () => {
+        // Create an object with the new data
+        const newData = {
+            Name: "geek", // Update with your data
+            Age: inputValue,
+            Date: "some-date",
+            programming: "python"
+        };
+
+        // Send POST request to update data
+        fetch('http://localhost:5000/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message); // Message from the server
+        });
+    };
 
     return (
         <div>
-          <h1>Welcome to the create user page</h1>
-          <Link to="/login">Go to the log in page</Link>
-    
-          <h1>Text Field Example</h1>
-          <form onSubmit={handleSubmit}> 
+            <h1>Welcome to the create user page</h1>
+            <Link to="/login">Go to the login page</Link>
+
+            <h1>Text Field Example</h1>
             <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              placeholder="Enter text..."
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Enter text..."
             />
-            <button type="submit">Submit</button> 
-          </form>
+
+            <button onClick={submitUser}>Submit</button>
         </div>
-      );
-    }
+    );
+}
 
 export default CreateUserPage;
