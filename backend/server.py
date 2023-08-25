f5c5353f3e224c67acf14187a2b1b0c4bff891dd
fathisahar <sahar.fathi@emailgmail.info>
2023-08-25 15:21:31 -0400

    data apis

--- backend/server.py
+++ backend/server.py
@@ -33,17 +33,46 @@ class RecipeIngredient(db.Model):
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
     quantity = db.Column(db.Float, nullable=False)
 
-
 @app.route('/add-category', methods=['POST'])
 def add_category():
     data = request.json
-    category_name = data.get('category')
+    category_name = data.get('nameCategory')  # Use 'nameCategory' as sent from front-end
 
     try:
         new_category = Category(name=category_name)
         db.session.add(new_category)
         db.session.commit()
         return jsonify(message='Category added successfully')
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/add-ingredient', methods=['POST'])
+def add_ingredient():
+    data = request.json
+    ingredient_name = data.get('nameIngredient')
+    category_id = data.get('categoryIngredient') 
+    stock_quantity = data.get('quantityIngredient') 
+
+    try:
+        new_ingredient = Ingredient(name=ingredient_name, category_id=category_id, stock_quantity=stock_quantity)
+        db.session.add(new_ingredient)
+        db.session.commit()
+        return jsonify(message='Ingredient added successfully')
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/add-recipe', methods=['POST'])
+def add_recipe():
+    data = request.json
+    name = data.get('nameRecipe')
+    instructions = data.get('instructionsRecipe')  
+    category_id = data.get('categoryRecipe')  
+
+    try:
+        new_recipe = Recipe(name=name, instructions=instructions, category_id=category_id)
+        db.session.add(new_recipe)
+        db.session.commit()
+        return jsonify(message='Recipe added successfully')
     except Exception as e:
         return jsonify(error=str(e))
 
--- src/P_Stock.js
+++ src/P_Stock.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import './CSS_Stock.css'; 
 
 function P_CreateUser() {
-    const [category, setCategory] = useState('');
+    const [nameCategory, setNameCategory] = useState('');
+    const [nameIngredient, setNameIngredient] = useState('');
+    const [nameRecipe, setNameRecipe] = useState('');
+    const [quantityIngredient, setQuantityIngredient] = useState('');
+    const [categoryIngredient, setCategoryIngredient] = useState('');
+    const [categoryRecipe, setCategoryRecipe] = useState('');
+    const [instructionsRecipe, setInstructionsRecipe] = useState('');
+
     //const history = useHistory();
 
-    const handleCategoryChange = (event) => {
-        setCategory(event.target.value);
-    };
+    const changeNameCategory = (event) => { setNameCategory(event.target.value);};
+    const changeNameIngredient = (event) => { setNameIngredient(event.target.value);};
+    const changeNameRecipe = (event) => { setNameRecipe(event.target.value);};
+    const changeQuantityIngredient = (event) => { setQuantityIngredient(event.target.value);};
+    const changeCategoryIngredient = (event) => { setCategoryIngredient(event.target.value);};
+    const changeCategoryRecipe = (event) => { setCategoryRecipe(event.target.value);};
+    const changeInstructionsRecipe = (event) => { setInstructionsRecipe(event.target.value);};
 
-    const handleSubmit = () => {
+
+    const handleCategorySubmit = () => {
         const newCategory = {
-            category: category
+            nameCategory: nameCategory
         };
 
-        // Send POST request to insert the new category
         fetch('http://localhost:5000/add-category', {
             method: 'POST',
             headers: {
@@ -24,28 +36,120 @@ function P_CreateUser() {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data.message); // Message from the server
+            console.log(data.message); 
             //history.push('/success'); // Redirect to success page
+            setNameCategory('');
+        });
+    };
+    
+    const handleIngredientSubmit = () => {
+        const newIngredient = {
+            nameIngredient: nameIngredient,        
+            quantityIngredient: quantityIngredient, 
+            categoryIngredient: categoryIngredient
+        };
+
+        fetch('http://localhost:5000/add-ingredient', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newIngredient)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message); 
+            setNameIngredient('');
+            setQuantityIngredient('');
+            setCategoryIngredient('');
+        });
+    };
+
+    const handleRecipeSubmit = () => {
+        const newIngredient = {
+            nameRecipe: nameRecipe,        
+            instructionsRecipe: instructionsRecipe, 
+            categoryRecipe: categoryRecipe
+        };
+
+        fetch('http://localhost:5000/add-recipe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newIngredient)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message); 
+            setNameRecipe('');
+            setCategoryRecipe('');
+            setInstructionsRecipe('');
         });
     };
 
     return (
         <div>
-            <h1>Create Category Page</h1>
-            <Link to="/login">Go to the login page</Link>
-
-            <h2>Create Category</h2>
-            <div>
-                <input
-                    type="text"
-                    value={category}
-                    onChange={handleCategoryChange}
-                    placeholder="Category"
-                />
-                <button onClick={handleSubmit}>Submit</button>
+            <div className="top">
+                <h1>insert information page</h1>
+                <Link to="/login">Go to the login page</Link>
+            </div>
+            <div className="container">
+                <div className="box">
+                    <input
+                        type="text"
+                        value={nameCategory}
+                        onChange={changeNameCategory}
+                        placeholder="Category"
+                    />
+                    <button onClick={handleCategorySubmit}>Submit</button>
+                </div>
+                <div className="box">
+                    <input
+                        type="text"
+                        value={nameIngredient}
+                        onChange={changeNameIngredient}
+                        placeholder="Name"
+                    />
+                    <input
+                        type="text"
+                        value={quantityIngredient}
+                        onChange={changeQuantityIngredient}
+                        placeholder="Quantity"
+                    />
+                    <input
+                        type="text"
+                        value={categoryIngredient}
+                        onChange={changeCategoryIngredient}
+                        placeholder="Category"
+                    />
+                    <button onClick={handleIngredientSubmit}>Submit</button>
+                </div>
+                <div className="box">
+                    <input
+                        type="text"
+                        value={nameRecipe}
+                        onChange={changeNameRecipe}
+                        placeholder="Name"
+                    />
+                    <input
+                        type="text"
+                        value={instructionsRecipe}
+                        onChange={changeInstructionsRecipe}
+                        placeholder="Instructions"
+                    />
+                    <input
+                        type="text"
+                        value={categoryRecipe}
+                        onChange={changeCategoryRecipe}
+                        placeholder="Category"
+                    />
+                    <button onClick={handleRecipeSubmit}>Submit</button>
+                </div>
             </div>
         </div>
     );
+    
 }
 
 export default P_CreateUser;
